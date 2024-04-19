75764121af6dfc20790654b572ba5f29012a15fd
yonosoytony <yonosoytony@duck.com>
2024-04-19 01:58:59 -0600

    Test query and put capabilities of the lambda function

--- backend/handleCSV/main.go
+++ backend/handleCSV/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -11,40 +16,79 @@ import (
 )
 
 type Transaction struct {
-	Email  string  `json:"email"`
-	Month  int     `json:"month"`
-	Amount float64 `json:"amount"`
+	ID        string  `json:"id"`
+	Email     string  `json:"email"`
+	Month     int     `json:"month"`
+	Amount    float64 `json:"amount"`
+	Timestamp string  `json:"timestamp"`
 }
 
-func HandleRequest(ctx context.Context) error {
+type RequestBody struct {
+	Email  string  `json:"email"`
+	Amount float64 `json:"amount"`
+	Month  int     `json:"month"`
+}
+
+func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var requestBody RequestBody
+	err := json.Unmarshal([]byte(event.Body), &requestBody)
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error unmarshalling request body: %s", err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: errorMsg}, nil
+	}
+
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
 	svc := dynamodb.New(sess)
 
+	// Generate a unique ID combining timestamp and email
+	timestamp := time.Now().Format(time.RFC3339)
+	id := fmt.Sprintf("%s-%s", timestamp, requestBody.Email)
+
 	transaction := Transaction{
-		Email:  "yonosoytony@duck.com",
-		Month:  7,
-		Amount: -17,
+		ID:        id,
+		Email:     requestBody.Email,
+		Month:     requestBody.Month,
+		Amount:    requestBody.Amount,
+		Timestamp: timestamp,
 	}
 
 	av, err := dynamodbattribute.MarshalMap(transaction)
 	if err != nil {
-		return err
+		errorMsg := fmt.Sprintf("Error marshalling transaction: %s", err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: errorMsg}, nil
 	}
 
 	input := &dynamodb.PutItemInput{
 		Item:      av,
-		TableName: aws.String("transactions"),
+		TableName: aws.String("stori-challenge-transactions"),
 	}
 
 	_, err = svc.PutItem(input)
 	if err != nil {
-		return err
+		errorMsg := fmt.Sprintf("Error putting item into DynamoDB: %s", err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: errorMsg}, nil
 	}
 
-	return nil
+	// Return a successful response
+	responseBody, err := json.Marshal(map[string]string{
+		"message": "Transaction successfully added",
+	})
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error marshalling response: %s", err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: errorMsg}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseBody),
+	}, nil
 }
 
 func main() {
--- backend/sendEmail/main.go
+++ backend/sendEmail/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -11,40 +15,89 @@ import (
 )
 
 type Transaction struct {
+	ID     string  `json:"id"`
 	Email  string  `json:"email"`
 	Month  int     `json:"month"`
 	Amount float64 `json:"amount"`
 }
 
-func HandleRequest(ctx context.Context) error {
+type RequestBody struct {
+	Email string `json:"email"`
+}
+
+func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var requestBody RequestBody
+	marshall_err := json.Unmarshal([]byte(event.Body), &requestBody)
+	if marshall_err != nil {
+		errorMsg := fmt.Sprintf("Error unmarshalling request body: %s", marshall_err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: errorMsg}, nil
+	}
+
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
 	svc := dynamodb.New(sess)
 
-	transaction := Transaction{
-		Email:  "yonosoytony@duck.com",
-		Month:  7,
-		Amount: -17,
+	var result *dynamodb.QueryOutput
+	var err error // Declare err here
+
+	if requestBody.Email != "" {
+		// Query by email
+		queryInput := &dynamodb.QueryInput{
+			TableName:              aws.String("stori-challenge-transactions"),
+			IndexName:              aws.String("EmailIndex"), // Ensure this matches your GSI name
+			KeyConditionExpression: aws.String("Email = :email"),
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":email": {
+					S: aws.String(requestBody.Email),
+				},
+			},
+		}
+		result, err = svc.Query(queryInput) // Assign new value to err
+	} else {
+		// Scan all transactions
+		scanInput := &dynamodb.ScanInput{
+			TableName: aws.String("stori-challenge-transactions"),
+		}
+		var scanResult *dynamodb.ScanOutput
+		scanResult, err = svc.Scan(scanInput) // Assign new value to err
+		if err != nil {
+			errorMsg := fmt.Sprintf("Error scanning transactions: %s", err.Error())
+			log.Println(errorMsg)
+			return events.APIGatewayProxyResponse{StatusCode: 500, Body: errorMsg}, nil
+		}
+		result = &dynamodb.QueryOutput{Items: scanResult.Items}
 	}
 
-	av, err := dynamodbattribute.MarshalMap(transaction)
 	if err != nil {
-		return err
+		errorMsg := fmt.Sprintf("Error querying transactions: %s", err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: errorMsg}, nil
 	}
 
-	input := &dynamodb.PutItemInput{
-		Item:      av,
-		TableName: aws.String("transactions"),
-	}
-
-	_, err = svc.PutItem(input)
+	// Unmarshal the result
+	var transactions []Transaction
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &transactions) // Assign new value to err
 	if err != nil {
-		return err
+		errorMsg := fmt.Sprintf("Error unmarshalling transactions: %s", err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: errorMsg}, nil
 	}
 
-	return nil
+	// Return the transactions
+	responseBody, err := json.Marshal(transactions) // Assign new value to err
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error marshalling response: %s", err.Error())
+		log.Println(errorMsg)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: errorMsg}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseBody),
+	}, nil
 }
 
 func main() {
